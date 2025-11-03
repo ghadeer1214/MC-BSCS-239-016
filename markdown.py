df9745c1622692552693f0e7eb4a52b466d10a0d
ghadeer1214 <ghadeeraskri1@gmail.com>
2025-11-03 13:36:36 +0500

    Add files via upload

---
+++ markdown.py
@@ -0,0 +1,22 @@
+print("hello world", 19)
+# i am learning python
+age=20
+print(age)
+name='Ghadeer'
+print(name)
+print(5+5)
+
+fruit=["A", "b", "C"]
+print(fruit)
+x, y, z= "apple", "banana", "orange"
+print(x, y, z)
+
+thisdict = {
+    "Uni": "Times University Multan",
+    "department": "BS Computer Science",
+    "Reg No": "MC-BSCS-239-016",
+    "Name": "Ghadeer Askri",
+    "semseter": 5,
+
+}
+print(thisdict)
